0ca08e56b613dab244c5ef82fabd2ad9f4e4c1b2
abiacaroline <abiamaro65@gmail.com>
2025-11-28 15:30:06 -0300

    cria funcao de fechar modal quando clicar no botao de fechar

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const cartModal = document.getElementById("cart-modal")
 const cartBtn = document.getElementById("cart-btn")
+const closeModalBtn = document.getElementById("close-modal-btn")
 
 cartBtn.addEventListener("click", () => {
   cartModal.style.display = "flex";
@@ -9,4 +10,8 @@ cartModal.addEventListener("click", (event) => {
   if (event.target === cartModal) {
     cartModal.style.display = "none";
   }
+});
+
+closeModalBtn.addEventListener("click", () => {
+  cartModal.style.display = "none";
 });
