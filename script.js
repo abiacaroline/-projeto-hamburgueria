b0fe40814ca35823ffa5c9fb926f8f633e39c0d9
abiacaroline <abiamaro65@gmail.com>
2025-11-28 15:26:03 -0300

    cria funcao de fechar modal quando clicar fora

--- script.js
+++ script.js
@@ -4,3 +4,9 @@ const cartBtn = document.getElementById("cart-btn")
 cartBtn.addEventListener("click", () => {
   cartModal.style.display = "flex";
 });
+
+cartModal.addEventListener("click", (event) => {
+  if (event.target === cartModal) {
+    cartModal.style.display = "none";
+  }
+});
