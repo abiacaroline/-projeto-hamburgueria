5f1e20672d7692bb1cd5b6d2daaef2f61c208aa0
abiacaroline <abiamaro65@gmail.com>
2025-12-03 10:32:38 -0300

    cria funcao para adicionar itens ao carrinho

--- script.js
+++ script.js
@@ -29,3 +29,17 @@ menu.addEventListener("click", (event) => {
     addToCart(name, price);
   }
 });
+
+function addToCart(name, price) {
+  const existingItem = cart.find((item) => item.name === name);
+
+  if (existingItem) {
+    existingItem.quantity += 1;
+  } else {
+    cart.push({
+      name,
+      price,
+      quantity: 1,
+    });
+  }
+}
