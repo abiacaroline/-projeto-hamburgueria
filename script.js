6a211edb64e63c00c1d039f6b54b90c8c4d3f056
abiacaroline <abiamaro65@gmail.com>
2025-12-03 11:59:42 -0300

    verifica horário e altera cor do card conforme status

--- script.js
+++ script.js
@@ -132,3 +132,20 @@ checkoutBtn.addEventListener("click", () => {
     addressInput.classList.add("border-red-500");
     return;
   }})
+
+function checkRestaurantOpen() {
+  const data = new Date()
+  const hora = data.getHours()
+  return hora >= 18 && hora < 22
+}
+
+const spanItem = document.getElementById("date-span")
+const isOpen = checkRestaurantOpen()
+
+if (isOpen) {
+  spanItem.classList.remove("bg-red-500")
+  spanItem.classList.add("bg-green-600")
+} else {
+  spanItem.classList.remove("bg-green-600")
+  spanItem.classList.add("bg-red-500")
+}
